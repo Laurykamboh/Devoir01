acd79d4e15fe837701a651aab1e771c17f54544e
De Song <34608120+babibou3@users.noreply.github.com>
2020-11-26 13:56:34 +0100

    C'est bon pour tes classes

--- CC01.BO/Ecole.cs
+++ CC01.BO/Ecole.cs
@@ -8,5 +8,29 @@ namespace CC01.BO
 {
     class Ecole
     {
+        public string Nom { get; set; }
+        public int Contact { get; set; }
+
+        public Ecole()
+        {
+
+        }
+
+        public Ecole(string nom, int contact)
+        {
+            Nom = nom;
+            Contact = contact;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Ecole ecole &&
+                   Nom == ecole.Nom;
+        }
+
+        public override int GetHashCode()
+        {
+            return 217408413 + EqualityComparer<string>.Default.GetHashCode(Nom);
+        }
     }
 }
--- CC01.BO/Etudiant.cs
+++ CC01.BO/Etudiant.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace CC01.BO
 {
+    [Serializable]
+
     public class Etudiant
     {
+       
+
+        public string Matricule { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Contact { get; set; }
+        public string Ecole { get; set; }
+
+        public Etudiant()
+        {
+
+        }
+
+        public Etudiant(string matricule, string nom, string prenom, int contact, string ecole)
+        {
+            Matricule = matricule;
+            Nom = nom;
+            Prenom = prenom;
+            Contact = contact;
+            Ecole = ecole;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                   Matricule == etudiant.Matricule;
+        }
+
+        public override int GetHashCode()
+        {
+            return 797189699 + EqualityComparer<string>.Default.GetHashCode(Matricule);
+        }
     }
 }
